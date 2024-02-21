3816dfb6bd7e93a8e51eef30ae8638754d42cd9f
Alessio Casablanca <159435390+Alessio76184@users.noreply.github.com>
2024-02-21 17:30:27 +0200

    Create merger_sort.py

---
+++ merger_sort.py
@@ -0,0 +1,44 @@
+def merge_sort(array):
+    ## ends the fuction if the array is less or equal to 1
+    if len(array) <= 1:
+        return
+    ##divide the left and right side
+    middle_point = len(array) // 2
+    ## creates a list from 0 to middle point using slice notation
+    left_part = array[:middle_point]
+    ## creates a list from middle point to the end of the list
+    right_part = array[middle_point:]
+
+    merge_sort(left_part)
+    merge_sort(right_part)
+
+    left_array_index = 0
+    right_array_index = 0
+    sorted_index = 0
+    ## the sorting loop in order of lowest to highest
+    while left_array_index < len(left_part) and right_array_index < len(right_part):
+        if left_part[left_array_index] < right_part[right_array_index]:
+            array[sorted_index] = left_part[left_array_index]
+            left_array_index += 1
+        else:
+            array[sorted_index] = right_part[right_array_index]
+            right_array_index += 1
+        sorted_index += 1
+
+    while left_array_index < len(left_part):
+        array[sorted_index] = left_part[left_array_index]
+        left_array_index += 1
+        sorted_index += 1
+    
+    while right_array_index < len(right_part):
+        array[sorted_index] = right_part[right_array_index]
+        right_array_index += 1
+        sorted_index += 1
+
+
+if __name__ == '__main__':
+    numbers = [4, 10, 6, 14, 2, 1, 8, 5]
+    print('Unsorted array: ')
+    print(numbers)
+    merge_sort(numbers)
+    print('Sorted array: '+ str(numbers))
